d14470d2093798c606afb226c8b857422d9543ac
unknown <mithu7ete@gmail.com>
2024-06-14 23:42:57 +0600

    reverse_string_video_150

--- src/ReverseStringPractice01.java
+++ src/ReverseStringPractice01.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ReverseStringPractice01 {
+public class ReverseStringPractice01 {
+    public static void main(String[] args) {
+        //String name = "Helen_Italian";
+        String reverseName = "";
+        String name = "StudentTeacher"
+        System.out.println(name.charAt(0));
+        System.out.println(name.length());
+        System.out.println("check index " + name.charAt(2));
+        System.out.println(name.indexOf(4));
+
+        for (int i = name.length() -1; i > 0 ; i--) {
+         reverseName = reverseName + name.charAt(i);
+
+        }
+    }
 }
